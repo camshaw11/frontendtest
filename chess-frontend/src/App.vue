cdda4f33346d78f16e591f51a0b10ba0ca9d609d
Cameron Shaw <cameron.shaw@commonledger.com>
2023-06-07 15:41:28 +0200

    get initial layout ready

---
+++ chess-frontend/src/App.vue
@@ -0,0 +1,30 @@
+
+<script setup>
+import ChessBoard from './components/ChessBoard.vue';
+</script>
+
+<template>
+  <header>
+    <h1>chessboard.</h1>
+  </header>
+  <main>
+    <div class="container">
+      <ChessBoard />
+      <div class="sidebar">sidebar here</div>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+.container {
+  border: 2px solid red;
+  width: 100%;
+  display: flex;
+  flex-flow: row wrap;
+}
+.sidebar {
+  border: 2px solid green;
+  min-height: 4em;
+}
+</style>
---
+++ chess-frontend/src/components/ChessBoard.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <div class="board">
+    board will be here
+  </div>
+</template>
+
+<style scoped>
+.board {
+  display: grid;
+  grid-gap: 0;
+  grid-template-columns: repeat(8, [col] 10vw);
+  grid-template-rows: repeat(8, [row] 10vw);
+  width: 80vw;
+  background-color: green;
+  justify-content: center;
+  align-content: center;
+  margin: 0 auto;
+}
+
+</style>
