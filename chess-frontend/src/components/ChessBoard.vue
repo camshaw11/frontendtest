143e43e7a23769310946893427923f2d48821c67
Cameron Shaw <cameron.shaw@commonledger.com>
2023-06-19 13:50:52 +0200

    dynamically generate squares using vue

--- chess-frontend/src/components/ChessBoard.vue
+++ chess-frontend/src/components/ChessBoard.vue
@@ -4,86 +4,39 @@ import { ref, defineEmits } from 'vue'
 const emit = defineEmits(['updateClickedSquareIds'])
 
 const clickedSquareIds = ref([])
+const clickedSquare = ref(null)
 
-const highlightSquare = (e) => {
-  const clickedSquare = e.target
-  // only target children
-  if (clickedSquare.classList.contains('square')) {
-    // toggle highlighted colour
-    clickedSquare.classList.toggle('highlighted')
-    // add new values to array
-    clickedSquareIds.value.push(clickedSquare.id)
-    // emit data to parent
-    emit('updateClickedSquareIds', clickedSquareIds.value)
-  }
+// handle selection of square on the board
+const selectSquare = (squareId) => {
+  clickedSquare.value = squareId
+  clickedSquareIds.value.push(squareId)
+  emit('updateClickedSquareIds', clickedSquareIds.value)
+}
+
+// check if square is currently clicked
+const isClickedSquare = (squareId) => {
+  return clickedSquare.value === squareId
+}
+
+// generate id of square based on the row and col position
+const getSquareId = (row, col) => {
+  const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+  const file = letters[col - 1]
+  const rank = 9 - row
+  return file + rank
 }
 </script>
 <template>
-  <div class="board" @click="highlightSquare">
-    <div class="square dark" id="a8"></div>
-    <div class="square light" id="b8"></div>
-    <div class="square dark" id="c8"></div>
-    <div class="square light" id="d8"></div>
-    <div class="square dark" id="e8"></div>
-    <div class="square light" id="f8"></div>
-    <div class="square dark" id="g8"></div>
-    <div class="square light" id="h8"></div>
-    <div class="square light" id="a7"></div>
-    <div class="square dark" id="b7"></div>
-    <div class="square light" id="c7"></div>
-    <div class="square dark" id="d7"></div>
-    <div class="square light" id="e7"></div>
-    <div class="square dark" id="f7"></div>
-    <div class="square light" id="g7"></div>
-    <div class="square dark" id="h7"></div>
-    <div class="square dark" id="a6"></div>
-    <div class="square light" id="b6"></div>
-    <div class="square dark" id="c6"></div>
-    <div class="square light" id="d6"></div>
-    <div class="square dark" id="e6"></div>
-    <div class="square light" id="f6"></div>
-    <div class="square dark" id="g6"></div>
-    <div class="square light" id="h6"></div>
-    <div class="square light" id="a5"></div>
-    <div class="square dark" id="b5"></div>
-    <div class="square light" id="c5"></div>
-    <div class="square dark" id="d5"></div>
-    <div class="square light" id="e5"></div>
-    <div class="square dark" id="f5"></div>
-    <div class="square light" id="g5"></div>
-    <div class="square dark" id="h5"></div>
-    <div class="square dark" id="a4"></div>
-    <div class="square light" id="b4"></div>
-    <div class="square dark" id="c4"></div>
-    <div class="square light" id="d4"></div>
-    <div class="square dark" id="e4"></div>
-    <div class="square light" id="f4"></div>
-    <div class="square dark" id="g4"></div>
-    <div class="square light" id="h4"></div>
-    <div class="square light" id="a3"></div>
-    <div class="square dark" id="b3"></div>
-    <div class="square light" id="c3"></div>
-    <div class="square dark" id="d3"></div>
-    <div class="square light" id="e3"></div>
-    <div class="square dark" id="f3"></div>
-    <div class="square light" id="g3"></div>
-    <div class="square dark" id="h3"></div>
-    <div class="square dark" id="a2"></div>
-    <div class="square light" id="b2"></div>
-    <div class="square dark" id="c2"></div>
-    <div class="square light" id="d2"></div>
-    <div class="square dark" id="e2"></div>
-    <div class="square light" id="f2"></div>
-    <div class="square dark" id="g2"></div>
-    <div class="square light" id="h2"></div>
-    <div class="square light" id="a1"></div>
-    <div class="square dark" id="b1"></div>
-    <div class="square light" id="c1"></div>
-    <div class="square dark" id="d1"></div>
-    <div class="square light" id="e1"></div>
-    <div class="square dark" id="f1"></div>
-    <div class="square light" id="g1"></div>
-    <div class="square dark" id="h1"></div>
+  <div class="board">
+    <div v-for="col in 8" :key="col" class="col">
+      <div
+        v-for="row in 8"
+        :key="row"
+        class="square"
+        :class="{ highlighted: isClickedSquare(getSquareId(row, col)) }"
+        @click="selectSquare(getSquareId(row, col))"
+      ></div>
+    </div>
   </div>
 </template>
 
@@ -97,18 +50,29 @@ const highlightSquare = (e) => {
   border: 1px solid white;
 }
 
-.dark {
+.square {
+  height: 10vw;
+}
+
+/* target every second column and colour the odd and even squares */
+.col:nth-child(even) .square:nth-child(odd) {
   background-color: var(--dark);
-  border: 1px solid white;
 }
-
-.light {
+.col:nth-child(even) .square:nth-child(even) {
   background-color: var(--light);
-  border: 1px solid white;
 }
 
-.highlighted {
-  background-color: var(--highlight);
+/* target every other column and colour the odd and even squares */
+.col:nth-child(odd) .square:nth-child(odd) {
+  background-color: var(--light);
+}
+
+.col:nth-child(odd) .square:nth-child(even) {
+  background-color: var(--dark);
+}
+
+.square.highlighted {
+  background-color: var(--highlight) !important;
 }
 
 /* styles for desktop screens */
@@ -116,6 +80,10 @@ const highlightSquare = (e) => {
   .board {
     grid-template-columns: repeat(8, [col] 5vw);
     grid-template-rows: repeat(8, [row] 5vw);
+  }
+
+  .square {
+    height: 5vw;
   }
 }
 </style>
